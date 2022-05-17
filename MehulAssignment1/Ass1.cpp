dd89cfc3d17df418610abca1d7285dd4da235362
MehulRajhans <105676990+MehulRajhans@users.noreply.github.com>
2022-05-17 11:16:30 +0530

    completed assignment 1

---
+++ MehulAssignment1/Ass1.cpp
@@ -0,0 +1,14 @@
+#include <cpp_api/navigation_bridge.h>
+
+Navigation nav;
+int main(int argc, char *argv[])
+{
+    std::cout<<"\nMission Started\n";
+    nav.take_off(5.0);   
+    nav.position_set(6.5,0,-5);
+    nav.position_set(6.5,6.5,-5);
+    nav.position_set(0,6.5,-5);
+    nav.position_set(0,0,-5);
+    nav.land(false);
+    std::cout<<"\nMission Completed\n";
+}
